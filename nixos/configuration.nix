89388597fc6a0aad014c561cff83dc972ab255a4
fantasygiveup <illia@danko.me>
2025-04-04 21:46:59 +0200

    Disable openssh

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -81,19 +81,6 @@
 
   programs.zsh.enable = true;
 
-  # This setups a SSH server. Very important if you're setting up a headless system.
-  # Feel free to remove if you don't need it.
-  services.openssh = {
-    enable = true;
-    settings = {
-      # Opinionated: forbid root login through SSH.
-      PermitRootLogin = "no";
-      # Opinionated: use keys only.
-      # Remove if you want to SSH using passwords
-      PasswordAuthentication = false;
-    };
-  };
-
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "24.11";
 }
