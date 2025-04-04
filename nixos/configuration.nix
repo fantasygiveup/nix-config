2cf2b8b906a3e9b0ea04836090c71de267c23c66
fantasygiveup <illia@danko.me>
2025-04-04 21:52:50 +0200

    Remove autogenerated comments

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,9 +1,6 @@
-# This is your system's configuration file.
-# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
 { inputs, outputs, lib, config, pkgs, ... }:
 
 {
-  # You can import other NixOS modules here
   imports = (builtins.attrValues outputs.nixosModules) ++ [
     inputs.hardware.nixosModules.common-cpu-amd
     inputs.hardware.nixosModules.common-pc-ssd
@@ -13,24 +10,11 @@
   ];
 
   nixpkgs = {
-    # You can add overlays here
     overlays = [
-      # Add overlays your own flake exports (from overlays and pkgs dir):
       outputs.overlays.additions
       outputs.overlays.modifications
       outputs.overlays.unstable-packages
-
-      # You can also add overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
     ];
-    # Configure your nixpkgs instance
     config = { allowUnfree = true; };
   };
 
