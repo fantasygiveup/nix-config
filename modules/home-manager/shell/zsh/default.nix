5c9cea8ceac52cde102e8cab8280036c42d96cd2
fantasygiveup <illia@danko.me>
2025-04-16 14:14:51 +0200

    Fix urlencode urldecode shell aliases

--- modules/home-manager/shell/zsh/default.nix
+++ modules/home-manager/shell/zsh/default.nix
@@ -13,10 +13,10 @@ in with lib; {
       defaultKeymap = "emacs";
       shellAliases = {
         urldecode =
-          "${pkgs.python3}/bin/python3 -c 'import sys, urlparse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
+          "${pkgs.python3}/bin/python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
 
         urlencode =
-          "${pkgs.python3}/bin/python3 -c 'import sys, urlparse as ul; print(ul.quote_plus(sys.stdin.read()))'";
+          "${pkgs.python3}/bin/python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
 
         e = "$EDITOR";
         bc =
