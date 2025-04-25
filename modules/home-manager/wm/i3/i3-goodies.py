760f08b0eaf500b11a8368b7d195d6681da44f8e
fantasygiveup <illia@danko.me>
2025-04-25 16:34:17 +0200

    Align the status line for tmux and i3blocks

--- modules/home-manager/wm/i3/i3-goodies.py
+++ modules/home-manager/wm/i3/i3-goodies.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-import i3ipc
+from i3ipc import Connection, Event
 import subprocess
 
-i3 = i3ipc.Connection()
+i3 = Connection()
 
 win_title = "wezterm"
 
@@ -31,5 +31,21 @@ def on_window_focus(i3, event):
             i3.command("bar mode dock")
 
 
-i3.on("window::focus", on_window_focus)
+def on_workspace_init(self, event):
+    if event.current:
+        if event.current.num != 1:
+            i3.command("bar mode dock")
+
+
+def on_workspace_focus(self, event):
+    if event.current:
+        i3.command("rename workspace \"{}\" to \"{}:{}\"".format(
+            event.current.name, event.current.num, " "))
+        i3.command("rename workspace \"{}\" to \"{}:{}\"".format(
+            event.old.name, event.old.num, " "))
+
+
+i3.on(Event.WINDOW_FOCUS, on_window_focus)
+i3.on(Event.WORKSPACE_INIT, on_workspace_init)
+i3.on(Event.WORKSPACE_FOCUS, on_workspace_focus)
 i3.main()
