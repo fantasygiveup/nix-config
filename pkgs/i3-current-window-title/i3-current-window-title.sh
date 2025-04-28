7ad92e4fa1152d6a5817701ea84e010b523c75cc
fantasygiveup <illia@danko.me>
2025-04-28 02:44:08 +0200

    Fix i3-current-window-title issue

--- pkgs/i3-current-window-title/i3-current-window-title.sh
+++ pkgs/i3-current-window-title/i3-current-window-title.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 while :; do
 	current_workspace_id="$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true) | .id')"
 	workspace_windows="$(i3-msg -t get_tree | jq -r ".nodes[].nodes[].nodes[] | select(.id == "$current_workspace_id")")"
-	IFS=$'\n' read -rd '' -a windows <<<"$(jq -r ".nodes[].name" <<<"$workspace_windows")"
 	focused_window="$(jq -r ".nodes[] | select(.focused == true)" <<<"$workspace_windows")"
-	window_pos="$(jq -r '.nodes | to_entries | map(select(.value.focused == true)) | .[0].key' <<<"$workspace_windows")"
-	((window_pos++))
-	focused_window_title="$(jq -r '.name' <<<"$focused_window")"
+	if [ -z "$focused_window" ]; then
+		workspace_windows="$(i3-msg -t get_tree | jq -r ".nodes[].nodes[].nodes[] | select(.id == "$current_workspace_id") | .nodes[]")"
+		focused_window="$(jq -r ".nodes[] | select(.focused == true)" <<<"$workspace_windows")"
+	fi
 
-	if [[ "${#windows[@]}" -lt 2 ]] || [[ "${#focused_window_title[@]}" -eq 0 ]]; then
+	window_index="$(jq -r '.nodes | to_entries | map(select(.value.focused == true)) | .[0].key | select(. != null)' <<<"$workspace_windows")"
+	window_pos="$((window_index + 1))"
+	focused_window_title="$(jq -r '.name' <<<"$focused_window")"
+	IFS=$'\n' read -rd '' -a windows <<<"$(jq -r ".nodes[].name" <<<"$workspace_windows")" || true
+
+	if [[ "${#windows[@]}" -lt 2 ]] || [[ -z "$focused_window_title" ]]; then
 		echo "$focused_window_title"
 	else
 		echo $focused_window_title": "$window_pos/${#windows[@]}
