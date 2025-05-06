53d306333d560a92161c3bc379f2adabcc01abb9
fantasygiveup <illia@danko.me>
2025-05-06 12:53:44 +0200

    Browsing: use firefox as a default browser, set profiles

--- modules/home-manager/profile/browsing/default.nix
+++ modules/home-manager/profile/browsing/default.nix
@@ -7,15 +7,37 @@ in with lib; {
 
   config = mkIf cfg.enable {
     nixpkgs.allowedUnfree = [ "google-chrome" "opera" ];
+    programs.firefox = {
+      enable = true;
+      policies = { DefaultDownloadDirectory = "\${home}/Downloads"; };
+      profiles = {
+        main = {
+          id = 0;
+          name = "main";
+        };
+        sirona = {
+          id = 1;
+          name = "sirona";
+        };
+        strong = {
+          id = 2;
+          name = "strong";
+        };
+        lux = {
+          id = 3;
+          name = "lux";
+        };
+      };
+    };
 
-    home.packages = with pkgs; [ google-chrome firefox opera lynx ];
+    home.packages = with pkgs; [ google-chrome opera lynx ];
 
     xdg.mimeApps.defaultApplications = {
-      "text/html" = [ "google-chrome.desktop" ];
-      "x-scheme-handler/http" = [ "google-chrome.desktop" ];
-      "x-scheme-handler/https" = [ "google-chrome.desktop" ];
-      "x-scheme-handler/about" = [ "google-chrome.desktop" ];
-      "x-scheme-handler/unknown" = [ "google-chrome.desktop" ];
+      "text/html" = [ "firefox.desktop" ];
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "x-scheme-handler/about" = [ "firefox.desktop" ];
+      "x-scheme-handler/unknown" = [ "firefox.desktop" ];
     };
   };
 }
