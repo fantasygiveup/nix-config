8941e56dee4c8787e8dedd8f466531835639f517
fantasygiveup <illia@danko.me>
2025-05-17 16:25:01 +0200

    I3: hide window border by adding gaps

--- modules/home/wm/i3/i3-hooks.py
+++ modules/home/wm/i3/i3-hooks.py
@@ -5,6 +5,22 @@ import subprocess
 i3 = Connection()
 
 
+def on_window_focus(i3, event):
+    focused_window = event.container
+    focused_workspace = i3.get_tree().find_focused().workspace()
+
+    if len(focused_workspace.leaves()) > 1:
+        i3.command("gaps top current set 0")
+    else:
+        i3.command("gaps top current set 2")
+
+
+def on_workspace_init(self, event):
+    if event.current:
+        if event.current.num != 1:
+            i3.command("bar mode dock")
+
+
 def on_workspace_focus(self, event):
     if event.current:
         i3.command("rename workspace \"{}\" to \"{}:{}\"".format(
@@ -13,5 +29,6 @@ def on_workspace_focus(self, event):
             event.old.name, event.old.num, "⚪"))
 
 
+i3.on(Event.WINDOW_FOCUS, on_window_focus)
 i3.on(Event.WORKSPACE_FOCUS, on_workspace_focus)
 i3.main()
