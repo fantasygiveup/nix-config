fantasygiveup/nix-config

commit bd98fff62a2fc80a7ec8efcab2b370d1bf49e59f

I3-blocks: add workspace-info

modules/home/wm/i3/default.nix
@@ -91,8 +91,15 @@ in with lib; {
 
     services.picom.enable = true;
     xdg.configFile."picom/picom.conf".source = ./picom/picom.conf;
+
     xdg.configFile."i3blocks/config".source =
       pkgs.substituteAll (mergeAttrs { src = ./i3blocks/config; } color);
+
+    xdg.configFile."i3blocks/workspace-info" = {
+      source = (pkgs.substituteAll
+        (mergeAttrs { src = ./i3blocks/workspace-info; } color));
+      executable = true;
+    };
 
     services.dunst = {
       enable = true;
@@ -156,6 +163,7 @@ in with lib; {
 
     home.packages = with pkgs; [
       (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" "Ubuntu" ]; })
+      (python3.withPackages (ppkgs: [ ppkgs.i3ipc ]))
       bemenu
       cliphist
       dconf-editor

modules/home/toolbox/fzf/notes-zsh/default.nix
@@ -17,6 +17,6 @@ in with lib; {
         export FZF_NOTES_PREVIEWER=~/.config/misc/fzf-notes-previewer
       '';
     };
-    home.packages = [ pkgs.fzf pkgs.python3 pkgs.ripgrep pkgs.zsh ];
+    home.packages = [ pkgs.fzf pkgs.ripgrep pkgs.zsh ];
   };
 }

modules/nixos/toolkit/core/default.nix
@@ -33,7 +33,6 @@ in with lib; {
 
       file
       home-manager
-      python3
       tree
       unstable.neovim
 

pkgs/default.nix
@@ -4,7 +4,6 @@ pkgs: rec {
   bemenu-commander = pkgs.callPackage ./bemenu-commander { };
   rofi-commander = pkgs.callPackage ./rofi-commander { };
   erun = pkgs.callPackage ./erun { };
-  i3-current-window-title = pkgs.callPackage ./i3-current-window-title { };
   i3-notification-status = pkgs.callPackage ./i3-notification-status { };
   i3blocks-xkb-layout-widget =
     pkgs.callPackage ./i3blocks-xkb-layout-widget { };

pkgs/i3-current-window-title/default.nix
@@ -1,34 +1,0 @@
-{ lib, stdenv, pkgs, }:
-stdenv.mkDerivation {
-  pname = "i3-current-window-title";
-  version = "1.0.0";
-  # Avoid the "> variable $src or $srcs should point to the source" error.
-  dontUnpack = true;
-  nativeBuildInputs = [ pkgs.makeWrapper ];
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p "$out/bin"
-    mkdir -p "$out/opt/i3-current-window-title"
-    substituteAll "${
-      ./i3-current-window-title.sh
-    }" "$out/bin/i3-current-window-title"
-    chmod a+x "$out/bin"/*
-    for i in $out/bin/*; do
-      wrapProgram "$i" \
-        --prefix PATH : "${
-          lib.makeBinPath [ pkgs.coreutils pkgs.bash pkgs.i3 ]
-        }"
-    done
-
-    runHook postInstall
-  '';
-
-  meta = {
-    description =
-      "Print the focused window's title with metadata at regular intervals for i3wm";
-    license = lib.licenses.mit;
-    platforms = lib.platforms.all;
-    homepage = "https://github.com/fantasygiveup/nix-config";
-  };
-}

modules/home/wm/i3/i3blocks/config
@@ -2,9 +2,9 @@ full_text=
 separator=false
 
 [wintitle]
-command=python3 ~/.config/i3blocks/workspace-info
-color=#@fg1@
-interval=persist
+command=~/.config/i3blocks/workspace-info
+markup=pango
+interval=1
 
 [separator]
 

modules/home/wm/i3/i3blocks/windows-info
@@ -1,26 +1,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-while :; do
-	current_workspace_id="$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true) | .id')"
-	workspace_windows="$(i3-msg -t get_tree | jq -r ".nodes[].nodes[].nodes[] | select(.id == "$current_workspace_id")")"
-	focused_window="$(jq -r ".nodes[] | select(.focused == true)" <<<"$workspace_windows")"
-	if [ -z "$focused_window" ]; then
-		workspace_windows="$(i3-msg -t get_tree | jq -r ".nodes[].nodes[].nodes[] | select(.id == "$current_workspace_id") | .nodes[]")"
-		focused_window="$(jq -r ".nodes[] | select(.focused == true)" <<<"$workspace_windows")"
-	fi
-
-	window_index="$(jq -r '.nodes | to_entries | map(select(.value.focused == true)) | .[0].key | select(. != null)' <<<"$workspace_windows")"
-	window_pos="$((window_index + 1))"
-	focused_window_title="$(jq -r '.name' <<<"$focused_window")"
-	IFS=$'\n' read -rd '' -a windows <<<"$(jq -r ".nodes[].name" <<<"$workspace_windows")" || true
-
-	if [[ "${#windows[@]}" -lt 2 ]] || [[ -z "$focused_window_title" ]]; then
-		echo "$focused_window_title"
-	else
-		echo $focused_window_title": "$window_pos/${#windows[@]}
-	fi
-
-	sleep 1
-done

modules/home/wm/i3/i3blocks/workspace-info
@@ -1,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+python3 <<END
+from i3ipc import Connection
+
+i3 = Connection()
+
+
+def mapper(x):
+    fg_color = "@fg1@"
+    if x.focused == True:
+        fg_color = "@g9@"
+    return "<span underline=\"single\" color=\"#@fg1@\" background=\"#@bg1@\" underline_color=\"#{}\">{}</span>".format(
+        fg_color, x.window_class)
+
+
+focused_workspace = i3.get_tree().find_focused().workspace()
+windows = list(map(mapper, focused_workspace.leaves()))
+print(" ".join(windows))
+END

commit fd80042f10a5d9f99080daf9908db67ff1ff0107

Fix issue introduced by commit bd98fff62a2f

modules/home/wm/i3/default.nix
@@ -169,7 +169,6 @@ in with lib; {
       dconf-editor
       gnome-screenshot
       gnome-tweaks
-      i3-current-window-title
       i3-notification-status
       i3blocks
       i3blocks-xkb-layout-widget