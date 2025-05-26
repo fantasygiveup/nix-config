fantasygiveup/nix-config

commit cfb42be58502080f3af4ece8777c4ddc5acf8ad4

I3: update color scheme; shrink workspace icons

modules/home/wm/i3/default.nix
@@ -192,7 +192,7 @@ in with lib; {
           rarrow=""
           larrow=""
 
-          pattern="<span foreground=\"%s\">$larrow<span background=\"%s\" color=\"%s\"> 💬 </span>$rarrow</span>\n"
+          pattern="<span foreground=\"%s\">$larrow<span background=\"%s\" color=\"%s\">💬</span>$rarrow</span>\n"
 
           widget() {
           	urgent="$(${pkgs.i3}/bin/i3-msg -t get_tree | ${pkgs.jq}/bin/jq '.. | select(.urgent? == true) | {id: .window, name: .name, class: .window_properties.class}')"

modules/home/profiles/global/default.nix
@@ -52,8 +52,8 @@ in with lib; {
       };
       theme = {
         name = mkMerge [
-          (mkIf (color.variant == "light") "Yaru")
-          (mkIf (color.variant == "dark") "Yaru-dark")
+          (mkIf (color.variant == "light") "Yaru-blue")
+          (mkIf (color.variant == "dark") "Yaru-blue-dark")
         ];
         package = pkgs.yaru-theme;
       };

modules/colors/catppuccin_latte.nix
@@ -34,8 +34,8 @@
       g5 = "f0d2a7";
       g6 = "bc0d33";
       g7 = "f8a8b9";
-      g8 = "f7ead3";
-      g9 = "e95420"; # from Yaru dark
+      g8 = "d20f39";
+      g9 = "f0d2a7";
     };
     description = ''
       Catppuccin Latte is the light theme variant.

modules/colors/catppuccin_mocha.nix
@@ -34,8 +34,8 @@
       g5 = "cf8d0c";
       g6 = "f6bacb";
       g7 = "ec2d62";
-      g8 = "9c6b0c";
-      g9 = "e95420"; # from Yaru dark
+      g8 = "d20f39";
+      g9 = "f0d2a7";
     };
     description = ''
       Catppuccin Mocha is the dark theme variant.

modules/home/toolbox/wezterm/wezterm.lua
@@ -13,9 +13,9 @@ end
 -- This is where you actually apply your config choices.
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-	local text = " ⚪ "
+	local text = "  "
 	if tab.is_active then
-		text = " 🟠 "
+		text = " ● "
 	end
 	return {
 		{ Foreground = { Color = "#@a1@" } },

modules/home/wm/i3/i3-hooks.py
@@ -24,9 +24,9 @@ def on_workspace_init(self, event):
 def on_workspace_focus(self, event):
     if event.current:
         i3.command("rename workspace \"{}\" to \"{}:{}\"".format(
-            event.current.name, event.current.num, "🟠"))
+            event.current.name, event.current.num, "●"))
         i3.command("rename workspace \"{}\" to \"{}:{}\"".format(
-            event.old.name, event.old.num, "⚪"))
+            event.old.name, event.old.num, ""))
 
 
 i3.on(Event.WINDOW_FOCUS, on_window_focus)

modules/home/wm/i3/i3/config
@@ -125,11 +125,11 @@ bindsym $mod+a focus parent
 
 # Define names for default workspaces for which we configure key bindings later on.
 # We use variables to avoid repeating the names in multiple places.
-set $ws1  "1:🟠"
-set $ws2  "2:🟠"
-set $ws3  "3:🟠"
-set $ws4  "4:🟠"
-set $ws10 "10:🟠"
+set $ws1   "1:󰧟"
+set $ws2   "2:󰧟"
+set $ws3   "3:󰧟"
+set $ws4   "4:󰧟"
+set $ws10 "10:󰧟"
 
 # switch to workspace
 bindsym $mod+1 workspace number $ws1

modules/home/wm/i3/i3blocks/config
@@ -26,7 +26,7 @@ interval=60
 markup=pango
 
 [notifications]
-command=~/.config/i3blocks/notifications '#@a1@' '#@g6@' '#@g9@' '#@g4@' '#@bg2@' '#@fg1@'
+command=~/.config/i3blocks/notifications '#@g8@' '#@g6@' '#@g9@' '#@g4@' '#@bg2@' '#@fg1@'
 interval=1
 markup=pango
 

commit d52641bfd1c69a240c92abab3c8415e1171620ae

Fix issue introduced by cfb42be58502

modules/home/wm/i3/default.nix
@@ -166,7 +166,7 @@ in with lib; {
           windows = i3.get_tree().find_focused().workspace().leaves()
           window = list(filter(mapper, windows))[0]
           length = len(windows)
-          accent_color = "${color.g9}" if length > 1 else "${color.fg1}"
+          accent_color = "${color.g8}" if length > 1 else "${color.fg1}"
           maybe_sep = " " if length > 1 else ""
 
           window_title = "<span color=\"#${color.fg1}\" style=\"italic\" font_size=\"small\" background=\"#${color.bg1}\">{}{}</span>".format(